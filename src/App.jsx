8fd71df77708b45f20e58434cf574bbf1e412fc3
muskan <muskantuteja714@gmail.com>
2025-07-03 23:31:24 +0530

    3/7/2025

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
@@ -8,28 +8,36 @@ import Campus from "./Components/Campus/Campus";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import VideoPlayer from "./Components/VideoPlayer/VideoPlayer";
 
 const App = () => {
+
+  const [playState, setPlayState] = useState(false);
   return (
     <div>
       <Navbar />
       <Hero />
       <div className="container">
-        <Title subTitle='Our PROGRAM' title='What WE Offer'/>
+        <Title subTitle="Our PROGRAM" title="What WE Offer" />
         <Programs />
-        <About></About>
-        <Title subTitle='Gallery' title='Campus Photos'/>
-        <Campus/>
-        <Title subTitle='TESTIMONIALS' title='What Student Says
-        '/>
-        <Testimonials/>
-         <Title subTitle='Contact Us' title='Get In Touch
-        '/>
-        <Contact/>
-        <Footer/>
-
-        
+        <About setPlayState={setPlayState}></About>
+        <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
+        <Title
+          subTitle="TESTIMONIALS"
+          title="What Student Says
+        "
+        />
+        <Testimonials />
+        <Title
+          subTitle="Contact Us"
+          title="Get In Touch
+        "
+        />
+        <Contact />
+        <Footer />
       </div>
+      <VideoPlayer playState={playState} setPlayState={setPlayState}/>
     </div>
   );
 };
